57abffaa320675baaa2d9cda84ff3f9d777e739c
Vladimir Odaysky <vldmrzp@gmail.com>
2025-03-16 13:48:39 +0200

    Initial commit from Create Next App

---
+++ zudoku.config.ts
@@ -0,0 +1,28 @@
+import type { ZudokuConfig } from "zudoku";
+
+const config: ZudokuConfig = {
+  topNavigation: [
+    { id: "docs", label: "Documentation" },
+    { id: "api", label: "API Reference" },
+  ],
+  sidebar: {
+    docs: [
+      {
+        type: "category",
+        label: "Overview",
+        items: ["introduction", "example"],
+      },
+    ],
+  },
+  redirects: [{ from: "/", to: "/docs/introduction" }],
+  apis: {
+    type: "file",
+    input: "./apis/openapi.yaml",
+    navigationId: "api",
+  },
+  docs: {
+    files: "/pages/**/*.{md,mdx}",
+  },
+};
+
+export default config;
