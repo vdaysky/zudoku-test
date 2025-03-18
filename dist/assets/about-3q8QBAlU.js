b282a20fbfdeabfe5cb31df6f3efd8cc83ab131e
Vladimir Odaysky <vldmrzp@gmail.com>
2025-03-18 16:53:25 +0200

    fix: include dist

---
+++ dist/assets/about-3q8QBAlU.js
@@ -0,0 +1,4 @@
+import{H as r,j as o}from"./entry.client-Dm5Wmzes.js";const u=[{depth:1,value:"About",id:"about",children:[{depth:2,value:"Author: Volodymyr Odaysky",id:"author-volodymyr-odaysky"},{depth:2,value:"Group: 8.1214",id:"group-81214"}]}],s=void 0;function e(t){const n={h1:"h1",h2:"h2",...r(),...t.components};return o.jsxs(o.Fragment,{children:[o.jsx(n.h1,{id:"about",children:"About"}),`
+`,o.jsx(n.h2,{id:"author-volodymyr-odaysky",children:"Author: Volodymyr Odaysky"}),`
+`,o.jsx(n.h2,{id:"group-81214",children:"Group: 8.1214"})]})}function a(t={}){const{wrapper:n}={...r(),...t.components};return n?o.jsx(n,{...t,children:o.jsx(e,{...t})}):e(t)}export{a as default,s as frontmatter,u as tableOfContents};
+//# sourceMappingURL=about-3q8QBAlU.js.map
